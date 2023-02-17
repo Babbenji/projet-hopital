d95ca2ac2c793353d2721669ad3b9a7a3ae7d8ef
aaronbrs <aaron.brosseau@etu-univ-orleans.fr>
2023-02-17 23:25:56 +0100

    Initialisation du microservice de gestion des RDV patients-personnel

---
+++ service-gestionRDVPatientsPersonnel/src/main/java/com/example/servicegestionrdvpatientspersonnel/ServiceGestionRdvPatientsPersonnelApplication.java
@@ -0,0 +1,13 @@
+package com.example.servicegestionrdvpatientspersonnel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceGestionRdvPatientsPersonnelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceGestionRdvPatientsPersonnelApplication.class, args);
+	}
+
+}
