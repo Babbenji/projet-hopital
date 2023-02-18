6d0c0f247dda875e951e55f4a42338536242f11b
El Ouariachi Rachida <rachida.el-ouariachi@etu.univ-orleans.fr>
2023-02-18 14:12:11 +0100

    init squelette service notification + ajout du fichier .gitignore global

---
+++ service-notification/src/main/java/fr/univ/orleans/miage/servicenotification/ServiceNotificationApplication.java
@@ -0,0 +1,13 @@
+package fr.univ.orleans.miage.servicenotification;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceNotificationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceNotificationApplication.class, args);
+    }
+
+}
---
+++ service-notification/src/test/java/fr/univ/orleans/miage/servicenotification/ServiceNotificationApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.univ.orleans.miage.servicenotification;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceNotificationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
