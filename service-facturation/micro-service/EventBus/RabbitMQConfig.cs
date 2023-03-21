c4754b36cd3581c9ed1ad2e54ccd6915b9a230f0
Solomon Lourdessamy <solomon.lourdessamy@etu.univ-orleans.fr>
2023-03-21 23:14:47 +0100

    ...

--- service-facturation/micro-service/EventBus/RabbitMQConfig.cs
+++ service-facturation/micro-service/EventBus/RabbitMQConfig.cs
@@ -6,7 +6,6 @@
         public string Host { get; set; } = string.Empty;
         public string Username { get; set; } = string.Empty;
         public string Password { get; set; } = string.Empty;
-
         public int Port { get; set; }
     }
 }
