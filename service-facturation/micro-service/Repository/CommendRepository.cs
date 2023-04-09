4e8a9a1a9f13c495d77e130fb4fe841c3a1c0e10
Solomon Lourdessamy <solomon.lourdessamy@etu.univ-orleans.fr>
2023-04-09 19:55:02 +0200

    rectification du bug serv facturation

---
+++ service-facturation/micro-service/Repository/CommendRepository.cs
@@ -0,0 +1,42 @@
+﻿using micro_service.Models;
+using MongoDB.Driver;
+
+namespace micro_service.Repository
+{
+    public class CommendRepository : ICommendRepository
+    {
+        private readonly IMongoCollection<Commande> commandes;
+
+        public CommendRepository(IMongoClient mongoClient)
+        {
+            IMongoDatabase database = mongoClient.GetDatabase("facturation");
+            this.commandes = database.GetCollection<Commande>("commande");
+        }
+
+        public Commande Create(Commande entity)
+        {
+            this.commandes.InsertOne(entity);
+            return entity;
+        }
+
+        public void Delete(int id)
+        {
+            this.commandes.DeleteOne(cmd => cmd.idCommande == id);
+        }
+
+        public List<Commande> GetAll()
+        {
+            return this.commandes.Find(cmd => true).ToList();
+        }
+
+        public Commande GetById(int id)
+        {
+            return this.commandes.Find(cmd => cmd.idCommande == id).SingleOrDefault();
+        }
+
+        public void Update(int id, Commande entity)
+        {
+            this.commandes.ReplaceOne(cmd => cmd.idCommande == id, entity);
+        }
+    }
+}
