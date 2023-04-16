426780c59c6ac29bce421f6c96b4b60464444335
El Ouariachi Rachida <rachida.el-ouariachi@etu.univ-orleans.fr>
2023-04-16 13:58:26 +0200

    finalisation de la config centralisée avec spring cloud config et vault, appliquer only sur auth et notif

--- run.sh
+++ run.sh
@@ -3,14 +3,14 @@
 
 # commande pour construire une image docker de chaque service spring boot
 
-#for dir in */ ; do
-#    if [ -e "$dir/pom.xml" ]; then
-#        echo "Building image for $dir"
-#        cd "$dir"
-#        mvn spring-boot:build-image -DskipTests
-#        cd ..
-#    fi
-#done
+for dir in */ ; do
+    if [ -e "$dir/pom.xml" ]; then
+        echo "Building image for $dir"
+        cd "$dir"
+        mvn spring-boot:build-image -DskipTests
+        cd ..
+    fi
+done
 
 
 # commande pour construite l'image docker du service .NET
@@ -18,7 +18,7 @@ docker build -t service-comptable:1.0 ./service-facturation
 
 
 # variable d'environnement
-export VAULT_TOKEN="token"
+export VAULT_TOKEN="token45"
 
 
 # commande pour construire et executer les containers docker
