35258ab7c3d731f7c4da21b0ad3110219679225b
El Ouariachi Rachida <rachida.el-ouariachi@etu.univ-orleans.fr>
2023-04-16 17:55:19 +0200

    deploiement prod opérationnel ^_^

---
+++ nettoyageDocker.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+docker compose down --rmi all --volumes --remove-orphans
+
+docker system prune -a -f
+
+#docker volume rm $(docker volume ls -q)
+
+# Stop and remove containers starting with "tp"
+#docker ps -a | grep 'tp' | awk '{print $1}' | xargs docker stop | xargs docker rm
--- run.sh
+++ run.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-
 # commande pour construire une image docker de chaque service spring boot
-
 for dir in */ ; do
     if [ -e "$dir/pom.xml" ]; then
         echo "Building image for $dir"
@@ -12,32 +10,14 @@ for dir in */ ; do
     fi
 done
 
+# variable d'environnement pour le token de vault
+export VAULT_TOKEN="token45"
 
 # commande pour construite l'image docker du service .NET
 docker build -t service-comptable:1.0 ./service-facturation
-
-
-# variable d'environnement
-export VAULT_TOKEN="token45"
-
 
 # commande pour construire et executer les containers docker
 docker compose build
 docker compose up
 
-
-
-
-
-# ancienne version :
-
-#
-#cd service-gestion-rdv-patients-personnel
-#mvn spring-boot:build-image -DskipTests
-
-# commande pour générer le fichier .jar de chaque service en ignorant les tests
-#./mvn clean package -DskipTests
-#mvn clean package -DskipTests
-# commande pour construire une image docker en ignorant les tests
-#mvn spring-boot:build-image -DskipTests
 
